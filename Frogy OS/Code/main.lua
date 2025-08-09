dd0325d76e9f6ea87a60529c8d3b643a0b3715e5
EpicLizard1000 <158706464+EpicLizard1000@users.noreply.github.com>
2025-08-09 11:38:20 +0100

    Update main.lua

--- Frogy OS/Code/main.lua
+++ Frogy OS/Code/main.lua
@@ -10,16 +10,18 @@ if cmd == "help" then
     print("quit = quits FrogyOS")
     print("lua = runs lua")
     print("te = text editor")
+    print("compile = compiles lua to a .fro file (FrogyOS executable file)")
+    print("run = run a .fro file (FrogyOS executable file)")
 end
 if cmd == "sys" then
     print("Version Number:  V1.00")
-    print("Version Name: FrogyOS BETA")
+    print("Version Name: FrogyOS Dawn")
 end
 if cmd == "quit" then
 return
 end
 if cmd == "lua" then
-    io.write("Filename")
+    io.write("Filename: ")
     local fn = io.read()
     dofile("U:/" .. fn)
 end
@@ -36,5 +38,28 @@ if cmd == "te" then
     file:write(cnt)
     file:close()
 
+end
+if cmd == "compile" then
+    io.write("Name of Lua file to compile (without file extension): ")
+    local cmp = io.read()
+    local infile = io.open("U:/" .. cmp .. ".lua", "r")
+    if not infile then
+    print("Could not open input file.")
+    return
+    end
+
+    local contents = infile:read("*a")
+    infile:close()
+
+    local cmpf = io.open("U:/" .. cmp .. ".fro", "w")
+    cmpf:write(contents)
+    cmpf:close()
+
+    print("Compiled " .. cmp .. ".lua to " .. cmp .. ".fro")
+end
+if cmd == "run" then
+    io.write("Name Of .fro file (no file extension): ")
+    local fron = io.read()
+    dofile("U:/" .. fron .. ".lua")
 end
 end
